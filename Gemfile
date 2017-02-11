2fc54e30b2073cea95765a3f8f6e8b686f0da0f1
Kamada Yasunobu <kamada@goukaku-suppli.com>
2017-02-11 18:34:08 +0900

    no message

--- Gemfile
+++ Gemfile
@@ -55,5 +55,9 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'mechanize'
 gem 'pry-rails'
 gem 'nokogiri'
+<<<<<<< HEAD
 
 gem 'rails_12factor', group: :production
+=======
+gem 'selenium-webdriver'
+>>>>>>> parent of e77c4cd... init commit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #resources :scraping
 
-  get '/', to: 'scraping#index'
+  get '/scraping', to: 'scraping#index'
   get '/scraping/show', to: 'scraping#show'
 
   get '/google', to: 'google#index'
