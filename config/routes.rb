98b889a5ede135481b3d561193d98f3c2df640a8
Kamada Yasunobu <kamada@goukaku-suppli.com>
2017-02-11 18:27:58 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #resources :scraping
 
-  get '/scraping', to: 'scraping#index'
+  get '/', to: 'scraping#index'
   get '/scraping/show', to: 'scraping#show'
 
   get '/google', to: 'google#index'
